b0a03cbdea7daa788fe74cb91958c38422c8660d
sumon092 <m.sumon@outlook.com>
2022-03-29 00:54:16 +0600

    router added

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h2>This about for us</h2>
+            <p>If you want to know,contact us</p>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/components/Friends/Friends.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Friends = () => {
+    return (
+        <div>
+            <h2>Hello, friends ,How are you???</h2>
+            <p><small>parle kichu taka dhar de.</small></p>
+        </div>
+    );
+};
+
+export default Friends;
